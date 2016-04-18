f873e4f8e1453a90c6287f6b96b89025afd35430
Jun Zheng <arthurjordan@gmail.com>
2016-04-19 09:58:47 +1000

    added descending sort

--- app/src/fancy_table.js
+++ app/src/fancy_table.js
@@ -13,7 +13,11 @@ export default React.createClass({
   },
 
   getInitialState: function(){
-    return { book_list: this.props.book_list };
+    return {
+      book_list: this.props.book_list,
+      sortby: null,
+      descending: false
+    };
   },
 
   _sort: function(event){
@@ -23,37 +27,56 @@ export default React.createClass({
       let book_list_copy = this.state.book_list.slice(); // a copy of the books array;
       console.log('book_list_copy is ', book_list_copy);
 
+      // descending is true if the index of the new column is the same as the currently sortby column and the sorting
+      // is not already descending;
+      let descending = this.state.sortby === column_index && !this.state.descending;
       // sor the book list
       switch(column_index){
         case 0:
           book_list_copy = book_list_copy.sort(function(a, b){
-              return a.name > b.name;
+              return descending
+              ? a.name < b.name
+              : a.name > b.name
           });
           break;
         case 1:
           book_list_copy = book_list_copy.sort(function(a, b){
-              return a.author > b.author;
+              return descending
+              ? a.author < b.author
+              : a.author > b.author
           });
           break;
         case 2:
           book_list_copy = book_list_copy.sort(function(a, b){
-              return a.language > b.language;
+              return descending
+              ? a.language < b.language
+              : a.language > b.language
           });
           break;
         case 3:
           book_list_copy = book_list_copy.sort(function(a, b){
-              return a.year - b.year;
+              return descending
+              ? a.year > b.year
+              : a.year < b.year
           });
           break;
         case 4:
           book_list_copy = book_list_copy.sort(function(a, b){
-              return a.sold > b.sold;
+              return descending
+              ? a.sold < b.sold
+              : a.sold > b.sold
           });
           break;
       }
 
       // after sorting, set the state with the new, sorted books;
-      this.setState({book_list: book_list_copy});
+      this.setState(
+        {
+          book_list: book_list_copy,
+          sortby: column_index,
+          descending: descending
+        }
+      );
       console.log('sorted book_list: ', book_list_copy);
   },
 
@@ -63,10 +86,13 @@ export default React.createClass({
 
   render: function(){
     let tableHeadNodes = this.props.headers.map(function(title, idx){
+        // if(this.state.sortby === idx){
+        //   title.arrow = this.state.descending ? '\u2191' : '\u2193'
+        // }
         return (
           <th key={idx}>{title.name}</th>
         )
-      });
+      }.bind(this));
 
     let tableBodyNodes = this.state.book_list.map(function(book, idx){
         return (
