fb2b65c89868df1c097050c00e134835d000f881
Jun Zheng <arthurjordan@gmail.com>
2016-04-18 17:11:17 +1000

    added sort column func

--- app/main.js
+++ app/main.js
@@ -14,13 +14,15 @@ let headers = [
 ]
 
 let books=[
-  {name: 'The Lord of the Rings', author: 'J.R.R Rowling', language: 'English', year: 1943, sold: '140 million'},
-  {name: 'Harry Porter', author: 'Jun Zheng', language: 'French', year: 1932, sold: '2000 million'},
-  {name: 'And Then there was none', author: 'Saint John', language: 'Spanish', year: 9233, sold: '923 thou'}
+  {name: 'A Nice Book', author: 'J.R.R Rowling', language: 'English', year: 1943, sold: '140 million'},
+  {name: 'Best Seller Harry Porter', author: 'Jun Zheng', language: 'French', year: 1932, sold: '2000 million'},
+  {name: 'Anroid And Then there was none', author: 'Saint John', language: 'Spanish', year: 9233, sold: '923 billion'},
+  {name: 'Awesome iOS App Development', author: 'John Doe', language: 'Italian', year: 19983, sold: '1583 million'},
+  {name: 'Far Far Away House', author: 'Nicole Best', language: 'Chinese', year: 2007, sold: '8583 million'}
     ];
 
 
 ReactDOM.render(
-  <FancyTable headers={headers} books={books}/>,
+  <FancyTable headers={headers} book_list={books}/>,
   document.getElementById('content')
 );
--- app/src/fancy_table.js
+++ app/src/fancy_table.js
@@ -3,9 +3,64 @@ import React from 'react';
 export default React.createClass({
   displayName: 'Fancy-Table',
 
-  getInitialState: function(){
-    return { data: this.props.books };
+  propTypes: {
+      headers: React.PropTypes.arrayOf(
+        React.PropTypes.string
+      ),
+      books: React.PropTypes.arrayOf(
+        React.PropTypes.string
+      )
   },
+
+  getInitialState: function(){
+    return { book_list: this.props.book_list };
+  },
+
+  _sort: function(event){
+    console.log('_sort is triggered');
+      let column_index = event.target.cellIndex; // get the index of which cell/column of the header which is clicked;
+      console.log('column is ', column_index);
+      let book_list_copy = this.state.book_list.slice(); // a copy of the books array;
+      console.log('book_list_copy is ', book_list_copy);
+
+      // sor the book list
+      switch(column_index){
+        case 0:
+          book_list_copy = book_list_copy.sort(function(a, b){
+              return a.name > b.name;
+          });
+          break;
+        case 1:
+          book_list_copy = book_list_copy.sort(function(a, b){
+              return a.author > b.author;
+          });
+          break;
+        case 2:
+          book_list_copy = book_list_copy.sort(function(a, b){
+              return a.language > b.language;
+          });
+          break;
+        case 3:
+          book_list_copy = book_list_copy.sort(function(a, b){
+              return a.year - b.year;
+          });
+          break;
+        case 4:
+          book_list_copy = book_list_copy.sort(function(a, b){
+              return a.sold > b.sold;
+          });
+          break;
+      }
+
+      // after sorting, set the state with the new, sorted books;
+      this.setState({book_list: book_list_copy});
+      console.log('sorted book_list: ', book_list_copy);
+  },
+
+  componentWillMount: function(){
+   this.setState({book_list: this.state.book_list});
+ },
+
   render: function(){
     let tableHeadNodes = this.props.headers.map(function(title, idx){
         return (
@@ -13,9 +68,9 @@ export default React.createClass({
         )
       });
 
-    let tableBodyNodes = this.props.books.map(function(book, idx){
+    let tableBodyNodes = this.state.book_list.map(function(book, idx){
         return (
-          <tr>
+          <tr key={idx}>
             <td>{book.name}</td>
             <td>{book.author}</td>
             <td>{book.language}</td>
@@ -26,7 +81,7 @@ export default React.createClass({
     });
     return (
       <table>
-        <thead>
+        <thead onClick={this._sort}>
             {tableHeadNodes}
         </thead>
         <tbody>
