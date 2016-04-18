5eb44c99610836f48c07699b9c94be5fc4938e19
Jun Zheng <arthurjordan@gmail.com>
2016-04-18 15:50:06 +1000

    made fancy_table component

--- app/main.js
+++ app/main.js
@@ -3,17 +3,29 @@ import '../public/css/style.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-let CommentBox = React.createClass({
-  render: function(){
-    return (
-      <div className='commentBox'>
-        <h1>Juns CommentBox 1213</h1>
-      </div>
-    )
-  },
-})
+import FancyTable from './src/fancy_table';
+
+let headers = [
+  {name: 'book'},
+  {name: 'author'},
+  {name: 'language'},
+  {name: 'published'},
+  {name: 'sales'}
+]
+
+let data=[
+        ["The Lord of the Rings", "J. R. R. Tolkien",
+        "English", "1954–1955", "150 million"],
+        ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry",  "French", "1943", "140 million"],
+        ["Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "107 million"],
+        ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"],
+        ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754–1791", "100 million"],
+        ["The Hobbit", "J. R. R. Tolkien", "English", "1937", "100 million"],
+        ["She: A History of Adventure", "H. Rider Haggard", "English", "1887", "100 million"]
+    ];
+
 
 ReactDOM.render(
-  <CommentBox />,
+  <FancyTable headers={headers} />,
   document.getElementById('content')
 );
