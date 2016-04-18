de43e85fd1714b5a0016db65614de27a424bfb90
Jun Zheng <arthurjordan@gmail.com>
2016-04-18 16:20:42 +1000

    built table head and table body

--- app/main.js
+++ app/main.js
@@ -13,19 +13,14 @@ let headers = [
   {name: 'sales'}
 ]
 
-let data=[
-        ["The Lord of the Rings", "J. R. R. Tolkien",
-        "English", "1954–1955", "150 million"],
-        ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry",  "French", "1943", "140 million"],
-        ["Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "107 million"],
-        ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"],
-        ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754–1791", "100 million"],
-        ["The Hobbit", "J. R. R. Tolkien", "English", "1937", "100 million"],
-        ["She: A History of Adventure", "H. Rider Haggard", "English", "1887", "100 million"]
+let books=[
+  {name: 'The Lord of the Rings', author: 'J.R.R Rowling', language: 'English', year: 1943, sold: '140 million'},
+  {name: 'Harry Porter', author: 'Jun Zheng', language: 'French', year: 1932, sold: '2000 million'},
+  {name: 'And Then there was none', author: 'Saint John', language: 'Spanish', year: 9233, sold: '923 thou'}
     ];
 
 
 ReactDOM.render(
-  <FancyTable headers={headers} />,
+  <FancyTable headers={headers} books={books}/>,
   document.getElementById('content')
 );
--- app/src/fancy_table.js
+++ app/src/fancy_table.js
@@ -1,21 +1,37 @@
 import React from 'react';
 
 export default React.createClass({
+  displayName: 'Fancy-Table',
 
-
+  getInitialState: function(){
+    return { data: this.props.books };
+  },
   render: function(){
-    let tableHeadNodes = this.props.headers.map(function(title){
+    let tableHeadNodes = this.props.headers.map(function(title, idx){
         return (
-          <th> {title.name}</th>
-
+          <th key={idx}>{title.name}</th>
         )
       });
 
+    let tableBodyNodes = this.props.books.map(function(book, idx){
+        return (
+          <tr>
+            <td>{book.name}</td>
+            <td>{book.author}</td>
+            <td>{book.language}</td>
+            <td>{book.year}</td>
+            <td>{book.sold}</td>
+          </tr>
+        )
+    });
     return (
       <table>
         <thead>
             {tableHeadNodes}
         </thead>
+        <tbody>
+          {tableBodyNodes}
+        </tbody>
       </table>
     )
   }
